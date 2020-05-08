06f6195d6604ecf1d654159f89215c7a071ae577
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-08 00:26:52 -0400

    fix follow bug

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -6,8 +6,10 @@ class FollowsController < ApplicationController
 
   def create
     # byebug
-    follow = Follow.find_or_create_by(follower_id: follow_params[:follower_id], followee_id: follow_params[:followee_id])
-    render json: follow, include: [:follower, :followee]
+    if (follow_params[:follower_id].to_i != follow_params[:followee_id].to_i)
+      follow = Follow.find_or_create_by(follower_id: follow_params[:follower_id],   followee_id: follow_params[:followee_id])
+      render json: follow, include: [:follower, :followee]
+    end
   end
 
   def filter
