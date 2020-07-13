30fec6109046f939b4fa84feddae0fac50954de5
CBreakr <github email address>
2020-07-12 20:49:03 -0400

    readme refers to the client

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_174817) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followee_id"
+
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
